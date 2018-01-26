6b87e8f773ec12ac38cc3a33ab11ceccf9bef2ea
Sergio <sgsaez92@gmail.com>
2018-01-26 22:13:45 +0100

    Add version definition in gradle

--- app/src/main/java/com/sgsaez/topgames/utils/Extensions.kt
+++ app/src/main/java/com/sgsaez/topgames/utils/Extensions.kt
@@ -7,8 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.sgsaez.topgames.TopGamesApplication
+import com.squareup.picasso.Picasso
 
 fun ViewGroup.inflate(layoutRes: Int): View =
         LayoutInflater.from(context).inflate(layoutRes, this, false)
@@ -17,7 +17,7 @@ val Fragment.topGamesApplication: TopGamesApplication
     get() = activity.application as TopGamesApplication
 
 fun ImageView.loadUrl(url: String) {
-    Glide.with(context).load(url).into(this)
+    Picasso.with(context).load(url).into(this)
 }
 
 fun isLollipopOrAbove(func: () -> Unit) {
