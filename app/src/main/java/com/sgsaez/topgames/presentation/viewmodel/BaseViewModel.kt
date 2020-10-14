bb7a1dc0880c76a789704b14ceb9cab7043ba347
sergio.garcia <sergio.garcia@intelygenz.com>
2020-10-14 20:53:35 +0200

    Remove boilerplate code in BaseViewModel

--- app/src/main/java/com/sgsaez/topgames/presentation/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/sgsaez/topgames/presentation/viewmodel/BaseViewModel.kt
@@ -3,13 +3,13 @@ package com.sgsaez.topgames.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sgsaez.topgames.support.domains.functional.Either
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 abstract class BaseViewModel<S>(baseState: Class<S>) : ViewModel() {
-
-    private val viewModelJob = SupervisorJob()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     private val _state = MutableLiveData<S>()
     val state: LiveData<S> get() = _state
@@ -25,14 +25,9 @@ abstract class BaseViewModel<S>(baseState: Class<S>) : ViewModel() {
     }
 
     protected fun <L, R> launchTask(action: () -> Either<L, R>, onCompleted: (Either<L, R>) -> Unit) {
-        uiScope.launch {
+        viewModelScope.launch {
             withContext(Dispatchers.IO) { action() }.let(onCompleted)
         }
-    }
-
-    override fun onCleared() {
-        viewModelJob.cancel()
-        super.onCleared()
     }
 
 }
